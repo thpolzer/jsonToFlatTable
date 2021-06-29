8c0f47d4811849b6550f93a8551b855203e7bc6b
Thomas Polzer <tpolzer@gmx.net>
2021-06-29 11:23:44 +0200

    flattening completed

--- converter/Transformer.py
+++ converter/Transformer.py
@@ -27,26 +27,48 @@ def flatten(jsondata,pairs,fathernode):
                 flatten(item,pairs,kvp[0])
     return pairs
 
-def flatten_update(jsondata,pairs,fathernode):
+def flatten_update(jsondata,pairs,fathernode,level):
     for kvp in jsondata.items():
         if type(kvp[1]) is not dict and type(kvp[1]) is not list:
             if fathernode is None:
-                pairs.append({kvp[0]:kvp[1]})
+                pairs.append({kvp[0]+"."+str(level):kvp[1]})
             else:
-                pairs.append({fathernode+"."+kvp[0]: kvp[1]})
+                pairs.append({fathernode+"."+kvp[0]+"."+str(level): kvp[1]})
         elif type(kvp[1]) is dict:
-                flatten(kvp[1],pairs,kvp[0])
+                flatten_update(kvp[1],pairs,kvp[0]+"."+str(level),0)
         elif type(kvp[1]) is list:
+            i = 0
             for item in kvp[1]:
-                flatten(item,pairs,kvp[0])
+                flatten_update(item,pairs,kvp[0]+"."+str(level),i)
+                i += 1
     return pairs
+
+
+def flatten_update1(jsondata,d,fathernode,level):
+    for kvp in jsondata.items():
+        if type(kvp[1]) is not dict and type(kvp[1]) is not list:
+            if fathernode is None:
+                d.update({kvp[0]+"."+str(level):kvp[1]})
+            else:
+                d.update({fathernode+"."+kvp[0]+"."+str(level): kvp[1]})
+        elif type(kvp[1]) is dict:
+                flatten_update1(kvp[1],d,kvp[0]+"."+str(level),0)
+        elif type(kvp[1]) is list:
+            i = 0
+            for item in kvp[1]:
+                flatten_update1(item,d,kvp[0]+"."+str(level),i)
+                i += 1
+    return d
 
 
 
 test = {"name":"Karl","amount":{"id":1,"name":"Anton"}}
 
 pairs = []
-result = flatten(data[0],pairs,None)
+d = {}
+#result = flatten(data[0],pairs,None)
+#result = flatten_update(data[0],pairs,None,0)
+result1 = flatten_update1(data[0],d,None,0)
 
 b = {"a":1}
 b.update({"b":2})
